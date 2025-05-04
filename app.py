ec850523b88137519674dd1c253c36f09d73df28
Riccardo <ricky2905@gmail.com>
2025-05-04 15:36:48 +0200

    fix

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for, abort
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
-import os
 
 app = Flask(__name__)
-
-DATABASE = os.path.join(os.path.dirname(__file__), 'libri.db')
+DB_FILE = 'libri.db'
 
 def get_db_connection():
-    conn = sqlite3.connect(DATABASE)
+    conn = sqlite3.connect(DB_FILE)
     conn.row_factory = sqlite3.Row
     return conn
 
@@ -18,43 +16,39 @@ def index():
     conn.close()
     return render_template('index.html', collezioni=collezioni)
 
-@app.route('/aggiungi_collezione', methods=['GET', 'POST'])
-def aggiungi_collezione():
-    if request.method == 'POST':
-        nome = request.form['nome']
-        if nome:
-            conn = get_db_connection()
-            conn.execute('INSERT INTO collezioni (nome) VALUES (?)', (nome,))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('index'))
-    return render_template('aggiungi_collezione.html')
-
 @app.route('/collezione/<int:id>')
 def collezione(id):
     conn = get_db_connection()
     collezione = conn.execute('SELECT * FROM collezioni WHERE id = ?', (id,)).fetchone()
     libri = conn.execute('SELECT * FROM libri WHERE collezione_id = ?', (id,)).fetchall()
     conn.close()
-    if collezione is None:
-        abort(404)
     return render_template('collezione.html', collezione=collezione, libri=libri)
 
-@app.route('/aggiungi_libro/<int:id>', methods=['GET', 'POST'])
-def aggiungi_libro(id):
+@app.route('/aggiungi_collezione', methods=['GET', 'POST'])
+def aggiungi_collezione():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        conn = get_db_connection()
+        conn.execute('INSERT INTO collezioni (nome) VALUES (?)', (nome,))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('index'))
+    return render_template('aggiungi_collezione.html')
+
+@app.route('/aggiungi_libro/<int:collezione_id>', methods=['GET', 'POST'])
+def aggiungi_libro(collezione_id):
     if request.method == 'POST':
         titolo = request.form['titolo']
         autore = request.form['autore']
-        if titolo and autore:
-            conn = get_db_connection()
-            conn.execute(
-                'INSERT INTO libri (titolo, autore, collezione_id) VALUES (?, ?, ?)',
-                (titolo, autore, id)
-            )
-            conn.commit()
-            conn.close()
-            return redirect(url_for('collezione', id=id))
-    return render_template('aggiungi_libro.html', collezione_id=id)
+        conn = get_db_connection()
+        conn.execute(
+            'INSERT INTO libri (titolo, autore, collezione_id) VALUES (?, ?, ?)',
+            (titolo, autore, collezione_id)
+        )
+        conn.commit()
+        conn.close()
+        return redirect(url_for('collezione', id=collezione_id))
+    return render_template('aggiungi_libro.html', collezione_id=collezione_id)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- init_db.py
+++ init_db.py
@@ -1,22 +1,21 @@
 import sqlite3
 
 conn = sqlite3.connect('libri.db')
-cur = conn.cursor()
 
-cur.execute('''
+conn.execute('''
 CREATE TABLE IF NOT EXISTS collezioni (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome TEXT NOT NULL
 )
 ''')
 
-cur.execute('''
+conn.execute('''
 CREATE TABLE IF NOT EXISTS libri (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     titolo TEXT NOT NULL,
     autore TEXT NOT NULL,
     collezione_id INTEGER NOT NULL,
-    FOREIGN KEY (collezione_id) REFERENCES collezioni(id)
+    FOREIGN KEY (collezione_id) REFERENCES collezioni (id)
 )
 ''')
 
